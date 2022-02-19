b108f09d80e926199e29ce2fb2a3ab72a252413c
moriarty83 <moriarty83@gmail.com>
2022-02-18 23:04:35 -0500

    solve

--- int32ToIp.js
+++ int32ToIp.js
@@ -1,23 +1,20 @@
 
 function int32ToIp(int32){
-    d = int32%100
-    console.log("d: " + d)
-    c = Math.floor(int32/100)
-    console.log("c: " + c)
+    let ip =`${Math.floor(int32/(2**24))}.${Math.floor((int32%(2**24))/(2**16))}.${Math.floor(((int32%(2**24))%(2**16))/2**8)}.${Math.floor(int32%(2**8))}`
+    return ip
   }
 
-  int32ToIp(3232235810)
-//192.168.1.34 == 
-// (32322358/256)-168
-// 10
-console.log(((192 * 256 + 168) * 256 + 1) * 256 + 32)
-// ((192 * 256 + 168) * 256 + 1) * 256 + 32)
+//   2149583361 ==> "128.32.10.1"
+  int32ToIp(2149583361)
 
-console.log(128*256)
-// describe("Tests", () => {
-//     it("test", () => {
-//   Test.assertEquals( int32ToIp(2154959208), "128.114.17.104", "wrong ip for interger: 2154959208") 
-//   Test.assertEquals( int32ToIp(0), "0.0.0.0", "wrong ip for integer: 0")
-//   Test.assertEquals( int32ToIp(2149583361), "128.32.10.1", "wrong ip for integer: 2149583361")
-//     });
-//   });
+// Cool solution:
+// function int32ToIp(int32){
+
+//     return (
+//              ((int32 >> 24) & 0xFF) + "." +
+//              ((int32 >> 16) & 0xFF) + "." +
+//              ((int32 >>  8) & 0xFF) + "." +
+//              ((int32) & 0xFF)
+//            );
+    
+//    }
